c54fe39eac8c8f76b064c6c11712e4add546f909
leonardo cortes <leodistrita@hotmail.com>
2024-01-10 12:29:15 -0500

    borrando archivos 1

--- app/Views/panel.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo $this->include('components/general/head');
-echo $this->include('components/panel/panel');
-echo $this->include('components/general/footer');
--- app/Views/registrosNuevo.php
+++
@@ -1,4 +0,0 @@
-<?php
-echo $this->include('components/general/head');
-echo $this->include('components/login/formnuevo');
-echo $this->include('components/general/footer');
